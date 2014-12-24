b2bafa9d213b5d563d1ca9bd496e633483e5cd4f
PanSours <pmc14h@hotmail.com>
2014-12-24 17:30:10 +0200

    XMLBuilder v.2

--- src/main/java/com/di/nomothesia/controller/XMLBuilder.java
+++ src/main/java/com/di/nomothesia/controller/XMLBuilder.java
@@ -35,22 +35,24 @@ public class XMLBuilder  {
  
             DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
             DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
+            docFactory.setNamespaceAware(true);
             Document doc = docBuilder.newDocument();
             
             //Root Element
             Element rootElement = doc.createElement("LegalDocument");
-            rootElement.setAttribute("URI", legald.getURI());
+            rootElement.setAttribute("id", legald.getId());
+            rootElement.setAttribute("documentURI", legald.getURI());
             doc.appendChild(rootElement);
 
             //Metadata Branch
             Element Metadata = doc.createElement("Metadata");
             rootElement.appendChild(Metadata);
             
-            Element title = doc.createElement("Title");
+            Element title = doc.createElementNS("http://dublincore.org/documents/dcmi-terms/","dc:title");
             title.setTextContent(legald.getTitle());
             Metadata.appendChild(title);
             
-            Element year = doc.createElement("Year");
+            Element year = doc.createElement("year");
             year.setTextContent(legald.getYear());
             Metadata.appendChild(year);
             
@@ -58,17 +60,30 @@ public class XMLBuilder  {
             FEK.setTextContent(legald.getFEK());
             Metadata.appendChild(FEK);
             
-            Element type = doc.createElement("DecisionType");
+            Element type = doc.createElement("decisiontype");
             type.setTextContent(legald.getDecisionType());
             Metadata.appendChild(type);
             
-            Element id = doc.createElement("Id");
-            id.setTextContent(legald.getId());
-            Metadata.appendChild(id);
-            
-            Element pdate = doc.createElement("PublicationDate");
+            Element pdate = doc.createElementNS("http://dublincore.org/documents/dcmi-terms/","dc:date");
             pdate.setTextContent(legald.getPublicationDate());
             Metadata.appendChild(pdate);
+            
+            Element  dctype = doc.createElementNS("http://dublincore.org/documents/dcmi-terms/","dc:type");
+            dctype.setTextContent("text");
+            Metadata.appendChild(dctype);
+            
+            Element  dcformat = doc.createElementNS("http://dublincore.org/documents/dcmi-terms/","dc:format");
+            dcformat.setTextContent("text/xml");
+            Metadata.appendChild(dcformat);
+            
+            Element  dclang = doc.createElementNS("http://dublincore.org/documents/dcmi-terms/","dc:language");
+            dclang.setTextContent("gr");
+            Metadata.appendChild(dclang);
+            
+            Element  dcident = doc.createElementNS("http://dublincore.org/documents/dcmi-terms/","dc:identifier");
+            dcident.setTextContent(legald.getURI());
+            Metadata.appendChild(dcident);
+            
             
             //Signers Branch
             Element signers = doc.createElement("ListOfSigners");
@@ -81,12 +96,12 @@ public class XMLBuilder  {
                 signers.appendChild(signer);
                 
                 //signers title
-                Element signtitle = doc.createElement("Title");
+                Element signtitle = doc.createElementNS("http://xmlns.com/foaf/spec/","foaf:title");
                 signtitle.setTextContent(legald.getSigners().get(i).getTitle());
                 signer.appendChild(signtitle);
                 
                 //signers fullname
-                Element signname = doc.createElement("FullName");
+                Element signname = doc.createElementNS("http://xmlns.com/foaf/spec/","foaf:name");
                 signname.setTextContent(legald.getSigners().get(i).getFullName());
                 signer.appendChild(signname);
                 
@@ -124,7 +139,8 @@ public class XMLBuilder  {
                 //article
                 Element article = doc.createElement("Article");
                 //article.setAttribute("number","" + legald.getArticles().get(i));
-                article.setAttribute("URI", legald.getArticles().get(i).getURI());
+                article.setAttribute("id", "" + legald.getArticles().get(i).getId());
+                article.setAttribute("documentURI", legald.getArticles().get(i).getURI());
                 rootElement.appendChild(article);
                 
                 //NOT IMPLEMNTED YET
@@ -133,21 +149,22 @@ public class XMLBuilder  {
                 //article.appendChild(artTitle);
                 
                 //article id
-                Element artId = doc.createElement("Id");
-                artId.setTextContent("" + legald.getArticles().get(i).getId());
-                article.appendChild(artId);
+                //Element artId = doc.createElement("Id");
+                //artId.setTextContent("" + legald.getArticles().get(i).getId());
+                //article.appendChild(artId);
                 
                 for (int j = 0;j<legald.getArticles().get(i).getParagraphs().size();j++) {
                     
                     //paragraph
                     Element paragraph = doc.createElement("Paragraph");
-                    paragraph.setAttribute("URI", legald.getArticles().get(i).getParagraphs().get(j).getURI());
+                    paragraph.setAttribute("id", "" + legald.getArticles().get(i).getParagraphs().get(j).getId());
+                    paragraph.setAttribute("documentURI", legald.getArticles().get(i).getParagraphs().get(j).getURI());
                     //paragraph.setAttribute("number","" + legald.getArticles().get(i).getParagraphs().get(j));
                     
                     //paragraph id
-                    Element parId = doc.createElement("Id");
-                    parId.setTextContent("" + legald.getArticles().get(i).getParagraphs().get(j).getId());
-                    paragraph.appendChild(artId);
+                    //Element parId = doc.createElement("Id");
+                    //parId.setTextContent("" + legald.getArticles().get(i).getParagraphs().get(j).getId());
+                    //paragraph.appendChild(parId);
                     
                     //NOT IMPLEMENTED YET
                     //Element parTable = doc.createElement("Table");
@@ -155,12 +172,15 @@ public class XMLBuilder  {
                     //paragraph.appendChild(parTable);
                     
                     //paragraph modification
-                    Element modification = doc.createElement("Modification");
-                    //modification.setAttribute("URI", legald.getArticles().get(i).getParagraphs().get(j).getModification().getURI());
-                    Element modType = doc.createElement("Type");
-                    //modType.setTextContent(legald.getArticles().get(i).getParagraphs().get(j).getModification().getType());
-                    modification.appendChild(modType);
-                    paragraph.appendChild(modification);
+                    if (legald.getArticles().get(i).getParagraphs().get(j).getModification() != null) {
+                        
+                        Element modification = doc.createElement("Modification");
+                        modification.setAttribute("documentURI", legald.getArticles().get(i).getParagraphs().get(j).getModification().getURI());
+                        paragraph.appendChild(modification);
+                        Element modType = doc.createElement("type");
+                        modType.setTextContent(legald.getArticles().get(i).getParagraphs().get(j).getModification().getType());
+                        modification.appendChild(modType);
+                    }
                     
                     Element list = doc.createElement("List");
                     
@@ -169,28 +189,30 @@ public class XMLBuilder  {
                         //paragraph case list
                         paragraph.appendChild(list);
                         Element pcase = doc.createElement("Case");
-                        pcase.setAttribute("URI", legald.getArticles().get(i).getParagraphs().get(j).getCaseList().get(n).getURI());
+                        pcase.setAttribute("id", "" + legald.getArticles().get(i).getParagraphs().get(j).getCaseList().get(n).getId());
+                        pcase.setAttribute("documentURI", legald.getArticles().get(i).getParagraphs().get(j).getCaseList().get(n).getURI());
                         list.appendChild(pcase);
                         
                         //case id
-                        Element casId = doc.createElement("Id");
-                        casId.setTextContent("" + legald.getArticles().get(i).getParagraphs().get(j).getCaseList().get(n).getId());
-                        pcase.appendChild(casId);
+                        //Element casId = doc.createElement("Id");
+                        //casId.setTextContent("" + legald.getArticles().get(i).getParagraphs().get(j).getCaseList().get(n).getId());
+                        //pcase.appendChild(casId);
                         
                         //case passages
                         for (int m = 0;m<legald.getArticles().get(i).getParagraphs().get(j).getCaseList().get(n).getPassages().size();m++) {
                         
                             //case passages
                             Element cpassage = doc.createElement("Passage");
-                            cpassage.setAttribute("URI", legald.getArticles().get(i).getParagraphs().get(j).getCaseList().get(n).getPassages().get(m).getURI());
+                            cpassage.setAttribute("id", "" + legald.getArticles().get(i).getParagraphs().get(j).getCaseList().get(n).getPassages().get(m).getId());
+                            cpassage.setAttribute("documentURI", legald.getArticles().get(i).getParagraphs().get(j).getCaseList().get(n).getPassages().get(m).getURI());
 
                             //case passage id
-                            Element caseId = doc.createElement("Id");
-                            caseId.setTextContent("" + legald.getArticles().get(i).getParagraphs().get(j).getCaseList().get(n).getPassages().get(m).getId());
-                            cpassage.appendChild(caseId);
+                            //Element caseId = doc.createElement("Id");
+                            //caseId.setTextContent("" + legald.getArticles().get(i).getParagraphs().get(j).getCaseList().get(n).getPassages().get(m).getId());
+                            //cpassage.appendChild(caseId);
 
                             //case passage text
-                            Element casText = doc.createElement("Text");
+                            Element casText = doc.createElement("text");
                             casText.setTextContent("" + legald.getArticles().get(i).getParagraphs().get(j).getCaseList().get(n).getPassages().get(m).getText());
                             cpassage.appendChild(casText);
 
@@ -204,15 +226,16 @@ public class XMLBuilder  {
                         
                         //paragraph passages
                         Element passage = doc.createElement("Passage");
-                        passage.setAttribute("URI", legald.getArticles().get(i).getParagraphs().get(j).getPassages().get(k).getURI());
+                        passage.setAttribute("id", "" + legald.getArticles().get(i).getParagraphs().get(j).getPassages().get(k).getId());
+                        passage.setAttribute("documentURI", legald.getArticles().get(i).getParagraphs().get(j).getPassages().get(k).getURI());
                         
                         //passage id
-                        Element pasId = doc.createElement("Id");
-                        pasId.setTextContent("" + legald.getArticles().get(i).getParagraphs().get(j).getPassages().get(k).getId());
-                        passage.appendChild(pasId);
+                        //Element pasId = doc.createElement("Id");
+                        //pasId.setTextContent("" + legald.getArticles().get(i).getParagraphs().get(j).getPassages().get(k).getId());
+                        //passage.appendChild(pasId);
                         
                         //passage text
-                        Element pasText = doc.createElement("Text");
+                        Element pasText = doc.createElement("text");
                         pasText.setTextContent("" + legald.getArticles().get(i).getParagraphs().get(j).getPassages().get(k).getText());
                         passage.appendChild(pasText);
                         
